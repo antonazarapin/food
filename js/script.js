696d146d7325ac6e4eb9eee3203f71580e1e704d
Anton Azarapin <108542363+antonazarapin@users.noreply.github.com>
2023-11-29 22:39:04 +0200

    Update timer deadline day

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     modal('[data-modal]', '.modal', modalTimerId);
-    timer('.timer', '2022-12-22');
+    timer('.timer', '2024-02-22');
     cards();
     calc();
     forms('form', modalTimerId);
